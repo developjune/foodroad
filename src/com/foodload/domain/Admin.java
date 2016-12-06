33d8f63bbb3693bd239b9a36762a2ce742d61e77
jsm <jsm@DESKTOP-JU2QROT>
2016-12-06 15:24:15 +0900

    test

--- src/com/foodload/domain/Admin.java
+++ src/com/foodload/domain/Admin.java
@@ -5,28 +5,28 @@ import java.io.Serializable;
 public class Admin implements Serializable {
 	private String id;
 	private String pw;
-	
+
 	public Admin() {
-		
+
 	}
-	
+
 	public Admin(String id, String pw) {
 		this.id = id;
 		this.pw = pw;
 	}
-	
+
 	public String getId() {
 		return id;
 	}
-	
+
 	public void setId(String id) {
 		this.id = id;
 	}
-	
+
 	public String getPw() {
 		return pw;
 	}
-	
+
 	public void setPw(String pw) {
 		this.pw = pw;
 	}
